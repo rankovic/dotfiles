667acb4483b42f5424c4051d13600811d496060e
Miroslav Ranković <m.rankovic@gmail.com>
2016-12-18 11:30:04 +0100

    add i3blocks

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ echo "Creating symlink for GIT in $HOME"
 
 rm $HOME/.gitconfig
 ln -s $DIR/git/gitconfig $HOME/.gitconfig
+
 rm $HOME/.gitignore_global
 ln -s $DIR/git/gitignore_global $HOME/.gitignore_global
 
@@ -18,3 +19,6 @@ ln -s $DIR/git/gitignore_global $HOME/.gitignore_global
 echo "Creating symlink for i3 in $HOME/.config/i3"
 rm $HOME/.config/i3/config
 ln -s $DIR/i3/config $HOME/.config/i3/config
+
+rm $HOME/.config/i3/i3blocks.conf
+ln -s $DIR/i3/i3blocks.conf $HOME/.config/i3/i3blocks.conf
